8de32b8ec0f48e5b4e6758df345e08c0fe8858e3
Roba-baba <boyr47@gmail.com>
2023-07-04 04:56:06 -0700

    printf project

---
+++ printf_integer.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * printf_integer - it prints a given intiger.
+ *
+ * @args: the number of args.
+ *
+ * @printed: the characters to be printed.
+ *
+ * Return: printed charcaters
+ */
+
+int printf_integer(va_list args, int printed)
+{
+	int num = va_arg(args, int);
+	int digits = 0;
+	int temp = num;
+	int digit;
+
+	if (num < 0)
+	{
+		printed += _putchar('-');
+		num = -num;
+
+		temp = num;
+	}
+
+	do {
+		digits++;
+		temp /= 10;
+	} while (temp != 0);
+
+	while (digits > 0)
+	{
+		int pow10 = 1;
+		int i;
+
+		for (i = 1; i < digits; i++)
+		{
+			pow10 *= 10;
+		}
+		digit = num / pow10;
+		printed += _putchar(digit + '0');
+		num -= digit * pow10;
+		digits--;
+	}
+	return (printed);
+}
---
+++ printf_pointer.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+#define MAX_HEX_DIGITS 16
+
+/**
+ * printf_pointer - this points at a binary number.
+ *
+ * @args: is a given number of args.
+ *
+ * @printed: characters being printed.
+ *
+ * Return: always printed charcaters.
+ */
+
+int printf_pointer(va_list args, int printed)
+{
+	void *ptr = va_arg(args, void*);
+	unsigned long num = (unsigned long) ptr;
+	int digits = 0;
+	int i;
+	unsigned long temp = num;
+	char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
+	char hex[MAX_HEX_DIGITS];
+
+	while (temp != 0)
+	{
+		digits++;
+		temp /= 16;
+	}
+
+	printed += _putchar('0');
+	printed += _putchar('x');
+
+	if (num == 0)
+	{
+		printed += _putchar('0');
+	}
+	else
+	{
+		for (i = digits - 1; i >= 0; i--)
+		{
+			int digit = num % 16;
+
+			hex[i] = hex_digits[digit];
+			num /= 16;
+		}
+		for (i = 0; i < digits; i++)
+		{
+			printed += _putchar(hex[i]);
+		}
+	}
+	return (printed);
+}
